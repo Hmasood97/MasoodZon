6a1340e0c78c0af87ae4470151259dfa90a552e0
Hmasood97 <hisham9977@gmail.com>
2020-01-04 15:33:24 -0500

    fifth

--- app_Folder/models.py
+++ app_Folder/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Book(models.Model):
+    id = models.CharField(max_length=20)
     title = models.CharField(max_length=250)
     author = models.CharField(max_length=250)
     genre = models.CharField(max_length=250)
--- app_Folder/views.py
+++ app_Folder/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from . models import Book
 # Create your views here.
 
 def home(request):
-    return render(request, 'home-page.html',)
+
+    harry_potter = Book()
+
+    return render(request, 'home-page.html', {'harry_potter': harry_potter})
